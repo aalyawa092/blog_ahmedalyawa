2a87f8185cce8577ccda0be21446a48d456f29e1
unknown <aalyawa092@wit-xdvm-057.MILLARDPS.NET>
2012-12-06 11:25:54 -0600

    more progress

--- actions/delete_post.php
+++ actions/delete_post.php
@@ -8,13 +8,6 @@ require('../config/db.php');
 // Connect to the database
 $conn = new mysqli('localhost',DB_USERNAME,DB_PASSWORD,DB_NAME);
 
-$sql = "SELECT post_title FROM post_id={$_GET['id']}";
-
-
-$results = $conn->query($sql);
-$band = $results->fetch_assoc();
-extract($post);
-
 // Construct query
 $sql = 'DELETE FROM posts WHERE post_id='.$_GET['id'];
 
@@ -26,7 +19,7 @@ if($conn->error != '') {
 	echo '<h2>MySQLError</h2><p>'.$conn->error.'</p>';
 	echo "<h3>SQL Executed</h3><p>$sql</p>";
 	echo '<pre>$_POST: ';
-	print_r($_POST);
+	print_r($_POST); 
 	echo '</pre>';
 } else {
 	// Redirect
--- views/admin/list_posts.php
+++ views/admin/list_posts.php
@@ -2,12 +2,27 @@
 
 require('config/db.php');
 
+$conn = new mysqli('localhost',DB_USERNAME,DB_PASSWORD,DB_NAME);
+
 $sql = "SELECT * FROM posts";
 
 $results = $conn->query($sql);
-	echo '<div id="">';
-		while($post = $results->fetch_assoc());
-			extract($post);
-				echo "<h2> $post_title</h2>";
-				echo "<p> $post_text </p>";
+
+echo "<h2>POSTS</h2>";
+
+echo '<table class="table">';
+while($post = $results->fetch_assoc()){
+	extract($post);
+	echo "<tr>";
+	echo "<td> $post_title</td>";
+	echo "<td> $post_text </td>";
+	echo "<td>";?>
+	<a class="btn btn-warning btn-mini" title="EDIT" href="./?p=admin/form_edit_post&amp;id=<?php echo $post['post_id']?>"><i class="icon-edit icon-white"></i></a>
+	<a class="btn btn-danger btn-mini" title="DELETE" href="actions/delete_post.php?id=<?php echo $post['post_id']?>"><i class="icon-trash icon-white"></i></a>
+	<?php
+	echo "</td>";
+	echo "</tr>";
+}
+echo '</table>';
 ?>
+	
